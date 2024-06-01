5a12954940a3af27a0b91cbb839b159c99fe1709
Harshit-Tiwari-5244 <147434248+Harshit-Tiwari-5244@users.noreply.github.com>
2024-06-01 22:32:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+{/* <div class="col-3 mt-5">
+                <div class="card">
+                    <img src="https://placehold.co/600x400/000000/FFF" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+
+                    </div>
+                </div>
+            </div>
+*/}
